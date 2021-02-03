b413f0ed3362d85c715e43c37703243e69d091bf
Xuan Yang <xuan.yang@goldenf.co>
2021-02-03 16:43:57 +0800

    init

---
+++ src/Contract/CQ9ServiceInterface.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+namespace GiocoPlus\BoleCard\Contract;
+/**
+ * CQ9 老虎機
+ */
+interface CQ9ServiceInterface {
+
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+
+    /**
+     * 遊戲詳情
+     * @param string $op_code
+     * @param string $account
+     * @param string $bet_id
+     * @return mixed
+     */
+    function gameDetail(string $op_code, string $account, string $bet_id);
+}
+
